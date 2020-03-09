c431f298146a3276ef57d74c493259f8fb53cb94
Joonsang <59277857+vanbasten97@users.noreply.github.com>
2020-03-09 13:55:05 +0900

    Create i-love-python.py

---
+++ O'Reilly/i-love-python.py
@@ -0,0 +1,9 @@
+# https://py.checkio.org/en/mission/i-love-python/
+# Degree : Elementary
+
+def i_love_python(degree:int):
+    if degree==0: return "I hate Python !!!!!!"
+    if 1<degree<=4: return "Other languages are better than Python"
+    if degree==5: return "Python? It's useful"
+    if 6<=degree<=9: return "I'm so happy thanks to Python"
+    if degree==10: return "I'm Python, Python is me !!!!"
