109dc4f5aab1bacdffc408a9f967d69ce7baa0d3
Joonsang <59277857+vanbasten97@users.noreply.github.com>
2020-03-09 09:55:28 +0900

    Update all-the-same.py

--- Home/all-the-same.py
+++ Home/all-the-same.py
@@ -5,3 +5,10 @@ from typing import List, Any
 
 def all_the_same(data:List[Any])->bool:
     return len(set(data)) <=1
+
+
+# Example)
+# all_the_same([1, 1, 1]) == True
+# all_the_same([1, 2, 1]) == False
+# all_the_same(['a', 'a', 'a']) == True
+# all_the_same([]) == True
