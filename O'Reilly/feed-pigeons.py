3aae068dc2fa77cbd73f7df12489332f1a93c947
Joonsang <59277857+vanbasten97@users.noreply.github.com>
2020-03-09 15:33:03 +0900

    Create feed-pigeons.py

---
+++ O'Reilly/feed-pigeons.py
@@ -0,0 +1,25 @@
+# https://py.checkio.org/en/mission/feed-pigeons/
+# Degree : Simple
+
+def checkio(portions):
+    fed = minute = pigeons = 0
+    while portions >= 0:
+        portions -= pigeons
+        minute += 1
+        print(minute)
+        if portions <= 0:
+            return fed
+        if minute < portions:
+            fed += minute
+            portions -= minute
+        else:
+            fed += portions
+            return fed
+        pigeons += minute
+    return fed
+
+# Example)
+# checkio(1) == 1
+# checkio(2) == 1
+# checkio(5) == 3
+# checkio(10) == 6
