4acbb3c5227d8ab2ae490f87832f057bf7486afb
Joonsang <59277857+vanbasten97@users.noreply.github.com>
2020-03-11 10:31:37 +0900

    Create say-hi.py

---
+++ Elementary/say-hi.py
@@ -0,0 +1,9 @@
+# https://py.checkio.org/en/mission/say-history/
+# Difficulty : Elementary
+
+def say_hi(name: str, age: int) -> str:
+    return "Hi. My name is {} and I'm {} years old".format(name,age)
+
+# Example)
+# say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old"
+# say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old"
